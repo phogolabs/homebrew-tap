5b7a2d93b9b1589e28fcc268fa440f9da96e3870
Svetlin Ralchev <svet@ralch.com>
2018-04-15 19:11:03 +0200

    Add Parcel formula

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # homebrew-tap
+
 Homebrew TAP for Phogo Labs Software
+
+Presently, the tap installs:
+
+- [parcel](https://github.com/phogolabs/parcel) - Golang Resournce Bundler
+
+```console
+$ brew tap phogolabs/tap
+$ brew install parcel
+```
---
+++ parcel.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'formula'
+require 'language/go'
+
+# Homebrew formula that install Parcel
+class Parcel < Formula
+  desc 'Golang Resource Bundler'
+  homepage 'https://phogolabs.github.io/opensource/parcel'
+  url 'https://github.com/phogolabs/parcel/archive/v0.1.tar.gz'
+  sha256 '73980ebe5cb054cca566d249ee2d2af4e06f5a6c075ba27861e3ef99cf5f55a7'
+  version '0.1'
+
+  depends_on 'go' => :build
+  depends_on 'glide' => :build
+
+  def install
+    ENV['GOPATH'] = buildpath
+    package_root = 'github.com/phogolabs'
+    package_root_dir = "#{buildpath}/src/#{package_root}"
+    package_name = "#{package_root}/parcel"
+    package_dir = "#{package_root_dir}/parcel"
+
+    mkdir_p package_root_dir
+    ln_sf buildpath, package_dir
+
+    Dir.chdir(package_dir) do
+      system 'glide', 'install'
+    end
+
+    system 'go', 'build', "#{package_name}/cmd/parcel"
+    bin.install 'parcel'
+  end
+
+  test do
+    version = pipe_output("#{bin}/parcel -v")
+    assert_match(/0.1/, version)
+  end
+end
