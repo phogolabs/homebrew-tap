f2e84b8355fa0cf456fa4fa10fae2f26f9cdc839
Svetlin Ralchev <svet@ralch.com>
2018-04-26 09:50:08 +0100

    Bump Parcello v0.8

--- parcello.rb
+++ parcello.rb
@@ -5,35 +5,41 @@ require 'language/go'
 
 # Homebrew formula that install Parcello
 class Parcello < Formula
-  desc 'Golang Resource Bundler'
+  desc 'Golang Resource Bundler and Embedder'
   homepage 'https://phogolabs.github.io/opensource/parcello'
-  url 'https://github.com/phogolabs/parcello/archive/v0.7.tar.gz'
-  sha256 '1c77a759e3ddac03753cdfc0e67bff39cad1485d6fb79dfd3a2e6c545930d40c'
-  version '0.7'
+  url 'https://github.com/phogolabs/parcello/archive/v0.8.tar.gz'
+  sha256 '32f7682f18a65a47a2e87a2b1b4adc9605421e17e739be447bcd867294d63b9a'
+  version '0.8'
 
   depends_on 'go' => :build
   depends_on 'glide' => :build
 
   def install
+    configure do |package_root, package_dir|
+      package_name = "#{package_root}/parcello"
+
+      Dir.chdir(package_dir) do
+        system 'glide', 'install'
+        system 'go', 'build', "#{package_name}/cmd/parcello"
+        bin.install 'parcello'
+      end
+    end
+  end
+
+  def configure
     ENV['GOPATH'] = buildpath
     package_root = 'github.com/phogolabs'
     package_root_dir = "#{buildpath}/src/#{package_root}"
-    package_name = "#{package_root}/parcello"
     package_dir = "#{package_root_dir}/parcello"
 
     mkdir_p package_root_dir
     ln_sf buildpath, package_dir
 
-    Dir.chdir(package_dir) do
-      system 'glide', 'install'
-    end
-
-    system 'go', 'build', "#{package_name}/cmd/parcello"
-    bin.install 'parcello'
+    yield package_root, package_dir
   end
 
   test do
     version = pipe_output("#{bin}/parcello -v")
-    assert_match(/0.7/, version)
+    assert_match(/0.8/, version)
   end
 end
