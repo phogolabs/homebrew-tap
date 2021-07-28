ef9a638d6e4c5dcb3634be8e0bd67f8ad2f11c85
goreleaserbot <goreleaser@carlosbecker.com>
2021-07-28 20:49:45 +0100

    Brew formula update for prana version v1.0.0-rc9

--- prana.rb
+++ prana.rb
@@ -5,28 +5,28 @@
 class Prana < Formula
   desc "Golang Database Management and Code Generation"
   homepage "https://github.com/phogolabs/prana"
-  version "1.0.0-rc8"
+  version "1.0.0-rc9"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/phogolabs/prana/releases/download/v1.0.0-rc8/prana_1.0.0-rc8_darwin_amd64.tar.gz"
-      sha256 "0410d740413a3506b669a770e4366bce3f7d7ee01823d350217bee4be2fa5414"
+      url "https://github.com/phogolabs/prana/releases/download/v1.0.0-rc9/prana_1.0.0-rc9_darwin_amd64.tar.gz"
+      sha256 "8f198a7a378bdeb3ed82a015dec35761fc543301723e3eaf75524e5c5a0c57cc"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/phogolabs/prana/releases/download/v1.0.0-rc8/prana_1.0.0-rc8_darwin_arm64.tar.gz"
-      sha256 "0f411dab4fa3711464683583203981260a4f58900cf193f9555b86120986eb6f"
+      url "https://github.com/phogolabs/prana/releases/download/v1.0.0-rc9/prana_1.0.0-rc9_darwin_arm64.tar.gz"
+      sha256 "f251264dd2365419661ceb35811f26f75871587f354571c76c0198a577354f93"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/phogolabs/prana/releases/download/v1.0.0-rc8/prana_1.0.0-rc8_linux_amd64.tar.gz"
-      sha256 "eef061bc65cf4b542469a905098287e768992d75d2bd53bd56b904a7a4b8c8f3"
+      url "https://github.com/phogolabs/prana/releases/download/v1.0.0-rc9/prana_1.0.0-rc9_linux_amd64.tar.gz"
+      sha256 "a6e47d34ff469ffbd86e4042329f02f4517e80e2d8af9a10c2e042e93d9da537"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/phogolabs/prana/releases/download/v1.0.0-rc8/prana_1.0.0-rc8_linux_arm64.tar.gz"
-      sha256 "f8fb911debb226a5b6a53d9bfab909f67257dc9507d5851aecd8aa1af2fa3325"
+      url "https://github.com/phogolabs/prana/releases/download/v1.0.0-rc9/prana_1.0.0-rc9_linux_arm64.tar.gz"
+      sha256 "29f8d16d28d6b5ee7ab189689ad25e5cf02883fedd9edd3d6620425b843c4a12"
     end
   end
 
